a8c3badc810d254eb0ceb6202874c5478034c90a
sujanmandana <sujanmandana@gmail.com>
2022-10-04 16:21:16 +0530

    commit

---
+++ file.js
@@ -0,0 +1,64 @@
+//arithematic program
+let a= 40;
+let b= 20;
+let c= a+b;
+let c1= a-b;
+let c2= a*b;
+let c3= a/b;
+let c4= a%b;
+console.log(c,c1,c2,c3,c4)
+
+//Relational operator   //now do not use same variable name for all programs
+var d= 10;
+var e= 30;
+var f= d>e;
+var f1= d<e;
+var f2= d<=e;
+var f3= d>=e;
+var f4= d|e;
+
+console.log(f,f1,f2,f3,f4)
+
+//Logical operator
+const g= 20;
+const h= 40;
+const i= g && h;
+const i1= g || h;
+const i2= g == h;
+const i3= !(g==h);
+console.log(i,i1,i2,i3)
+
+//Assignment operator
+let j= 50;
+let k= 30;
+let l= j=k;
+let l1= j+=k;
+let l2= j*=k;
+console.log(l,l1,l2)
+
+//Increment and decrement
+var m= 10;
+var n= 20;
+var o= ++m;
+var o1= --n;
+console.log(o,o1)
+
+// conditional operator
+let p= 20;
+let q= 30;
+let r= p>q ? true:false;
+let r1= p<q ? true:false;
+console.log(r,r1)
+
+//bitwise operator
+let s= 10;
+let t= 20;
+let u= s&t;
+let u1= ~s;
+let u2= ~t;
+let u3= s^t;
+let u4= s>>t;
+let u5= t<<s;
+console.log(u,u1,u2,u3,u4,u5)
+
+
